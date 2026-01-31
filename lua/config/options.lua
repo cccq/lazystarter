8b9aad44d06ecb4ca1b860a02954545926b4e04f
cccq <geminicq5@outlook.com>
2026-01-31 21:36:58 +0800

    feat: add neovide configuration

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,19 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- neovide configuration
+if vim.g.neovide then
+  local os_name = vim.uv.os_uname().sysname
+  if os_name == "Windows_NT" then
+    vim.o.guifont = "JetBrainsMono Nerd Font Mono:h10"
+  elseif os_name == "Darwin" then
+    vim.o.guifont = "JetBrainsMono Nerd Font Mono:h13"
+  else
+    vim.o.guifont = "JetBrainsMono Nerd Font Mono:h12"
+  end
+  vim.o.linespace = 3
+  vim.g.neovide_refresh_rate = 60
+  vim.g.neovide_confirm_quit = true
+  vim.g.neovide_cursor_vfx_mode = "railgun"
+end
